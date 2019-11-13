7d5e110f40684d4646d1d94e841ab221a2f9155e
Havid <great_havid@yahoo.co.id>
2019-11-13 11:42:20 +0700

    Tugas 15 JS

---
+++ tugas15.js
@@ -0,0 +1,17 @@
+// ====== Tugas 15 JavaScript =====
+
+function diagonal() {
+    var a = 8;
+    var b = 6;
+    var c = Math.pow(c,2);
+  
+    console.log('Nilia AB : ', a);
+    console.log('Nilai BC : ', b);
+  
+    c = Math.pow(a,2) + Math.pow(b,2);
+    c = Math.sqrt(c);
+  
+    console.log('Panjang sisi AC pada segitiga siku-siku tersebut adalah : ', c, 'cm')
+  }
+  
+  diagonal()
